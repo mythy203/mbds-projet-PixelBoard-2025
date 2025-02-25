daf79bab6309c7f5aa3b8e51c034bfffe4a6aa94
jtnsamuel <jtn.samuel@gmail.com>
2025-02-25 14:35:12 +0100

    feat(api) : update user model

--- packages/api/models/users.js
+++ packages/api/models/users.js
@@ -7,13 +7,6 @@ const userSchema = new mongoose.Schema({
         unique: true,
         trim: true,
     },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true,
-        trim: true,
-    },
     password: {
         type: String,
         required: true,
